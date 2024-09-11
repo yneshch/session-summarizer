d89a63d036cfabd2c33310671f09f8767ba7d451
Yakov N <ynescher@gmail.com>
2024-09-10 23:16:05 -0400

    fix to constants

--- backend/constants.py
+++ backend/constants.py
@@ -11,6 +11,8 @@ IS_WINDOWS_MACHINE = os.name == 'nt'
 if IS_WINDOWS_MACHINE:
     BASE_PATH = Path(os.getenv("BASE_PATH")).as_posix()
     TRANSCRIPT_PATH = Path(os.getenv("TRANSCRIPT_PATH")).as_posix()
-
+else:
+    BASE_PATH = Path(os.getenv("BASE_PATH"))
+    TRANSCRIPT_PATH = Path(os.getenv("TRANSCRIPT_PATH"))
 DESIRED_SUMMARY_NAME = os.getenv("DESIRED_SUMMARY_NAME", "summary")
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", None)
